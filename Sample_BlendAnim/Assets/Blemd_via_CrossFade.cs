7417e568ca2d245e715aeed69217fa3a71c017af
hidenori tumurai <hidenori tumurai>
2016-07-30 17:57:56 +0900

    first commit.

---
+++ Sample_BlendAnim/Assets/Blemd_via_CrossFade.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blemd_via_CrossFade : MonoBehaviour
+{
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    float deltaTime;
+    bool morfDirection = false;
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+        if (deltaTime > 1.0f)
+        {
+            deltaTime = 0;
+            morfDirection = !morfDirection;
+            if (morfDirection)
+            {
+                anim.CrossFade("Piece_Blend_C", 0);
+            }
+            else
+            //anim.CrossFade("Run");
+            {
+
+            }
+        }
+    }
+}
---
+++ Sample_BlendAnim/Assets/Blemd_via_Mecanim.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blemd_via_Mecanim : MonoBehaviour {
+    public Animation anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    float deltaTime;
+    bool morfDirection = false;
+	void Update () {
+        deltaTime += Time.deltaTime;
+        if (deltaTime > 1.0f)
+        {
+            deltaTime = 0;
+            morfDirection = !morfDirection;
+            if (morfDirection)
+            {
+                //anim.CrossFade("Piece_Blend");
+            }
+            else
+                //anim.CrossFade("Run");
+            {
+
+            }
+        }
+    }
+}
